10b30b25e8549434bbc1b9b6c4e04661e0f9fa44
Simphiwe Dlamini <s20035@pjwstk.edu.pl>
2020-03-06 11:16:31 +0100

    first commit

---
+++ Tutorial 1 solution/Tutorial 1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Tutorial_1
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var websiteURL = args[0];
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(websiteURL);
+
+            if (response.IsSuccessStatusCode) {
+                var htmlContent = await response.Content.ReadAsStringAsync();
+
+                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+",RegexOptions.IgnoreCase);
+
+                var emailAdresses = regex.Matches(htmlContent);
+
+                foreach (var match in emailAdresses) {
+                    Console.WriteLine(match.ToString());
+                }
+            }
+
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Kiss My Ass World");
+            var var1 = "Yo mama";
+        }
+
+    }
+}
